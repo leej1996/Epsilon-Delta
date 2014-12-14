089c9c53984e3cebb5258b4da174cbab76d159c9
Nagee Elghassein <nag3el@gmail.com>
2014-12-14 00:09:38 -0500

    added quadratic.

--- delta_ep.py
+++ delta_ep.py
@@ -1,4 +1,4 @@
-class Proof:
+class Linear:
 
     def __init__(self, limit, equation):
         """Precondition: equation is a str in form n * x,
@@ -21,6 +21,7 @@ class Proof:
 
     def multiply(self):
         l = self.no_x.split('*')
+
         answer = int(l[0]) * int(l[1])
         self.answer = answer
         self.coefficient = l[0]
@@ -33,17 +34,50 @@ class Proof:
         c = int(splits[1]) / int(self.coefficient)
         self.factored = '{0} * ({1} - {2})'.format(self.coefficient, x, str(int(c)))
 
+class Quadratic:
+    def __init__(self, limit, equation):
+        """Precondition: equation is a str in form n ** x,
+         where x is a variable and n is an int"""
+        self.goesto = limit
+        self.fx = equation
+    def __str__(self):
+        return 'For all e > 0, there exists d > 0 : 0 < |x - {0}| < d -> |{1} - {2}| < e'.format(self.goesto, self.fx, self.answer)
+    def multiply(self):
+        answer = int(self.goesto) ** 2
+        self.answer = answer
+        #self.coefficient = 1
+    def factor(self):
+        a = '{0} - {1}'.format(self.fx, str(self.answer))
+        splits = a.split(' - ')
+        self.factored = '|x + {0}| * |x - {0}|'.format(self.goesto)
+        self.bound = 2*int(self.goesto) + 1
+
 
 if __name__ == '__main__':
     limit = str(input('What does x go to?: '))
     equation  = input('What is the equation?: ')
-    a = Proof(limit, equation)
-    a.plug_in()
-    a.multiply()
-    a.factor()
-    print (str(a))
-    print ('choose d = e/{0}'.format(a.coefficient))
-    print ('Proof:')
-    print ('\t 0 < |x - {0}| < d'.format(a.goesto))
-    print ('\t 0 < |x - {0}| < e/{1}'.format(a.goesto, a.coefficient))
-    print ('\t |{0} - {1}| < e'.format(a.fx, a.answer))
+    if '**' in equation:
+        b = Quadratic(limit, equation)
+        b.multiply()
+        b.factor()
+        print (str(b))
+        print ('Scratch:')
+        print ('\tTo set a bound on x, notice that |{0} - {1}| = {2}'.format(b.fx,b.goesto, b.factored))
+        print ('\tTake d = 1')
+        print ('\t{0}|x - {1}|'.format(b.bound, b.goesto))
+        print ('\tThen we can choose d = e/{0}'.format(b.bound))
+        print ('Proof:')
+        print ('Choose d = min(1, e/{0})'.format(b.bound))
+        print ('\t|{0} - {1}| = {2}'.format(b.fx, b.answer, b.factored))
+        print ('\t           <= {0} * |x - {1}| <= {0}(e/{0}) = e'.format(b.bound, b.goesto))
+    else:
+        a = Linear(limit, equation)
+        a.plug_in()
+        a.multiply()
+        a.factor()
+        print (str(a))
+        print ('Proof:')
+        print ('choose d = e/{0}'.format(a.coefficient))
+        print ('\t 0 < |x - {0}| < d'.format(a.goesto))
+        print ('\t 0 < |x - {0}| < e/{1}'.format(a.goesto, a.coefficient))
+        print ('\t |{0} - {1}| < e'.format(a.fx, a.answer))
