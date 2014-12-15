8834a27b7dc127daa443ecff84a474bcecb5d17a
Nagee Elghassein <nag3el@gmail.com>
2014-12-15 18:29:31 -0500

    fixed linear to take nx+c

--- delta_ep.py
+++ delta_ep.py
@@ -1,37 +1,75 @@
 class Linear:
 
     def __init__(self, limit, equation):
-        """Precondition: equation is a str in form n * x,
-         where x is a variable and n is an int"""
-        self.goesto = limit
+        """(int, str) -> Linear
+
+        >>> l = Linear(4, '2*x')
+        >>> l.limit
+        4
+        >>> l.fx
+        '2*x'
+        >>> l.answer
+        8
+        """
+        self.limit = limit
         self.fx = equation
 
-    def __str__(self):
-        return 'For all e > 0, there exists d > 0 : 0 < |x - {0}| < d -> |{1} - {2}| < e'.format(self.goesto, self.fx, self.answer)
-
-    def plug_in(self):
-
-        no_x = ''
+        answer = ''
         for ch in self.fx:
-            if ch.isalpha():
-                no_x += str(self.goesto)
+            if ch == 'x':
+                answer += str(self.limit)
             else:
-                no_x += ch
-        self.no_x = no_x
+                answer += ch
+        x = eval(answer)
 
-    def multiply(self):
-        l = self.no_x.split('*')
-        answer = int(l[0]) * int(l[1])
-        self.answer = answer
-        self.coefficient = l[0]
+        self.answer = x
+
+        self.make_factor()
+        self.factor()
+        if self.factored[0].isdigit():
+            self.delta = 'e/{0}'.format(self.factored[0])
+        else:
+            self.delta = 'e'
+
+        #self.delta = 'e/{0}'.format(self.factored[0])
+
+
+
+    def __str__(self):
+        return 'For all e > 0, there exists d > 0 : 0 < |x - {0}| < d -> |{1} - {2}| < e'.format(self.limit, self.fx, self.answer)
+
+    def make_factor(self):
+        w = '{0} - {1}'.format(self.fx, self.answer)
+        if '-' in self.fx:
+            x = w.split('-')
+        elif '+' in self.fx:
+            x = w.split('+')
+            x[1] = eval(x[1])
+        else:
+            x = w
+        to_factor = ''
+        for s in x:
+            to_factor += str(s)
+        self.to_factor = to_factor
 
     def factor(self):
-        a = '{0} - {1}'.format(self.fx, str(self.answer))
-        splits = a.split(' - ')
-        split_x = splits[0].split('*')
-        x = split_x[1]
-        c = int(splits[1]) / int(self.coefficient)
-        self.factored = '{0} * ({1} - {2})'.format(self.coefficient, x, str(int(c)))
+        factored = ''
+        divide_by = self.to_factor[0]
+        factored += divide_by + '('
+        for n in self.to_factor:
+            if n.isdigit():
+                x = str(int(int(n)/int(divide_by)))
+
+                if x == '1':
+                    factored += ''
+                else:
+                    factored += x
+            else:
+                factored += n
+        q = factored + ')'
+        self.factored = q.replace('*', '')
+
+
 class Quadratic:
     def __init__(self, limit, equation):
         """Precondition: equation is a str in form n ** x,
@@ -51,44 +89,6 @@ class Quadratic:
         self.factored = '|x + {0}| * |x - {0}|'.format(self.goesto)
         self.bound = 2*int(self.goesto) + 1
 
-
 if __name__ == '__main__':
-    print('Multiplication symbol must be expressed with a *, ** for powers')
-    print('Quadratics must be written in from x**n, where n is an integer')
-    print('Linear equations must be written in form n*x, where n is an integer')
-    print('=================================================================')
-    limit = str(input('What does x go to?: '))
-    equation  = input('What is the equation?: ')
-    print('=================================================================')
-    if '**' in equation:
-        b = Quadratic(limit, equation)
-        b.multiply()
-        b.factor()
-        print ('Specific Definition:')
-        print (str(b))
-        print('=================================================================')
-        print ('Scratch:')
-        print ('\tTo set a bound on x, notice that |{0} - {1}| = {2}'.format(b.fx,str(b.answer), b.factored))
-        print ('\tTake d = 1')
-        print ('\t{0}|x - {1}|'.format(b.bound, b.goesto))
-        print ('\tThen we can choose d = e/{0}'.format(b.bound))
-        print('=================================================================')
-        print ('Proof:')
-        print ('Choose d = min(1, e/{0})'.format(b.bound))
-        print ('\t|{0} - {1}| = {2}'.format(b.fx, b.answer, b.factored))
-        print ('\t           <= {0} * |x - {1}| <= {0}(e/{0}) = e'.format(b.bound, b.goesto))
-        print ('QED')
-    else:
-        a = Linear(limit, equation)
-        a.plug_in()
-        a.multiply()
-        a.factor()
-        print ('Specific Definition:')
-        print (str(a))
-        print('=================================================================')
-        print ('Proof:')
-        print ('choose d = e/{0}'.format(a.coefficient))
-        print ('\t 0 < |x - {0}| < d'.format(a.goesto))
-        print ('\t 0 < |x - {0}| < e/{1}'.format(a.goesto, a.coefficient))
-        print ('\t |{0} - {1}| < e'.format(a.fx, a.answer))
-        print ('QED')
+    import doctest
+    doctest.testmod()
