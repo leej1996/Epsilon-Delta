093b8431842ae61f28e7bd071a8791da8ddfea34
Nagee Elghassein <nag3el@gmail.com>
2014-12-23 14:06:50 -0800

    more stuff yo

--- stuff.py
+++ stuff.py
@@ -1,12 +1,27 @@
-from flask import Flask
+import flask, flask.views
 import run_delta_ep
+import delta_ep
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.secret_key = "delta"
 
-@app.route('/')
-def hello_world():
-    return 'hello worls'
+class View(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
+
+    def post(self):
+        limit =  eval(flask.request.form['expression'])
+        fx = eval(flask.request.form['equation'])
+        flask.flash(limit)
+        flask.flash(fx)
+        if '**' in fx:
+            q = run_delta_ep.Quadratic(fx, limit)
+        else:
+            q = run_delta_ep.Linear(fx, limit)
+        return self.get()
+
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
 
-if __name__ == '__main__':
-    app.run()
+app.debug = True
+app.run()
