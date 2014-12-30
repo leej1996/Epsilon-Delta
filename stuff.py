eb8f46e48a0e5e5e6070698368e9e00c44cb7ce5
Nagee Elghassein <nag3el@gmail.com>
2014-12-30 15:46:16 -0800

    tbh, i don't even.

--- stuff.py
+++ stuff.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 #================================
-from flask import render_template, request
+from flask import render_template, request, url_for
 from run_delta_ep import proof
 @app.route('/')
 def index():
